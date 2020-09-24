44750942e21ad6b0eb11bb2a974b0ceac538e343
zaferuzun <uznzafer@gmail.com>
2020-09-24 20:43:07 +0300

    commit create project

---
+++ libraryproject/src/main/java/com/zenontechnology/libraryproject/LibraryprojectApplication.java
@@ -0,0 +1,13 @@
+package com.zenontechnology.libraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryprojectApplication.class, args);
+	}
+
+}
